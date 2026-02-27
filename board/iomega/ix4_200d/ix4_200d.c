358384fff57a53c10c1ed2a05680db3b29e2918d
Tony Dinh <mibodhi@gmail.com>
2026-02-26 18:44:36 -0800

    Iomage ix4-200d - Initial version

---
+++ board/iomega/ix4_200d/ix4_200d.c
@@ -0,0 +1,142 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2022-2026  Tony Dinh <mibodhi@gmail.com>
+ *
+ */
+
+#include <netdev.h>
+#include <asm/arch/cpu.h>
+#include <asm/arch/soc.h>
+#include <asm/arch/mpp.h>
+#include <asm/io.h>
+#include <asm/arch/gpio.h>
+#include <asm/mach-types.h>
+#include <init.h>
+#include <linux/bitops.h>
+#include <init.h>
+#include <linux/bitops.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+/* Stock MPPs
+
+Marvell>> md.l 0xF1010000 8
+f1010000: 01111111 00003311 33331140 33003333    .....3..@.3333.3
+f1010010: 00000033 00000000 00000000 00000000    3...............
+
+Stock OE val
+
+Marvell>> md.l 0xF1010100 1
+f1010100: 1000d080    ....
+
+Marvell>> md.l 0xF1010140 1
+f1010140: 0001000c    ....
+*/
+
+/*
+ * Those values and defines are taken from stock version
+
+	1     		0     0     0     	d       	    	0     	8     	0
+        0001  		0000  0000  0000  	1101			0000  	1000  	0000
+	gpio 28         			gpio 15,14,12       		gpio 7
+
+	000	1		000	c
+              	0001			1100
+		gpio 16			gpio 3,2
+*/
+
+/* GPIO configuration */
+#define IX4_200D_OE_LOW		(~(0))
+#define IX4_200D_OE_HIGH	(~(0))
+
+#define IX4_200D_OE_VAL_LOW \
+        (BIT(7) | BIT(12) | BIT(14) | BIT(15) | BIT(28))
+#define IX4_200D_OE_VAL_HIGH \
+        (BIT(2) | BIT(3) | BIT(16))
+
+int board_early_init_f(void)
+{
+	/* Multi-Purpose Pins Functionality configuration */
+
+//f1010000: 01111111 00003311 33331140 33003333    .....3..@.3333.3
+//f1010010: 00000033 00000000 00000000 00000000    3...............
+
+// Incororate the above MPPs to kwmpp_config[]
+
+	static const u32 kwmpp_config[] = {
+		MPP0_NF_IO2,
+		MPP1_NF_IO3,
+		MPP2_NF_IO4,
+		MPP3_NF_IO5,
+		MPP4_NF_IO6,
+		MPP5_NF_IO7,
+		MPP6_SYSRST_OUTn,	/* Reset signal */
+		MPP7_GPO,
+		MPP8_TW_SDA,
+		MPP9_TW_SCK,
+		MPP10_UART0_TXD,
+		MPP11_UART0_RXD,
+		MPP12_GPO,
+		MPP13_GPIO,
+		MPP14_GPIO,
+		MPP15_GPIO,
+		MPP16_GPIO,
+		MPP17_SATA0_PRESENTn,
+		MPP18_NF_IO0,
+		MPP19_NF_IO1,
+		MPP20_GE1_0,
+		MPP21_GE1_1,
+		MPP22_GE1_2,
+		MPP23_GE1_3,
+		MPP24_GE1_4,
+		MPP25_GE1_5,
+		MPP26_GE1_6,
+		MPP27_GE1_7,
+		MPP28_GPIO,
+		MPP29_GPIO,
+		MPP30_GE1_10,
+		MPP31_GE1_11,
+		MPP32_GE1_12,
+		MPP33_GE1_13,
+		MPP34_GPIO,
+		MPP35_GPIO,
+		MPP36_GPIO,
+		MPP37_GPIO,
+		MPP38_GPIO,
+		MPP39_GPIO,
+		MPP40_GPIO,
+		MPP41_GPIO,
+		MPP42_GPIO,
+		MPP43_GPIO,
+		MPP44_GPIO,
+		MPP45_GPIO,
+		MPP46_GPIO,
+		MPP47_GPIO,
+		MPP48_GPIO,
+		MPP49_GPIO,
+		0
+	};
+	kirkwood_mpp_conf(kwmpp_config, NULL);
+	return 0;
+
+}
+
+int board_eth_init(struct bd_info *bis)
+{
+	return cpu_eth_init(bis);
+}
+
+int board_init(void)
+{
+	/* address of boot parameters */
+	gd->bd->bi_boot_params = mvebu_sdram_bar(0) + 0x100;
+
+	return 0;
+}
+
+int board_late_init(void)
+{
+	/* Do late init to ensure successful enumeration of devices on PCI bus */
+	pci_init();
+	return 0;
+}
